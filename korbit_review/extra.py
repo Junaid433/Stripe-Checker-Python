469eaf05f411196f9ca0ebc2bd351b925b3fa1ff
Junaid Rahman <junaid.cloud2@gmail.com>
2025-08-14 13:27:04 +0600

    Delete korbit_review directory

--- korbit_review/extra.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def hit_sender(card,message,chat_id):
-    bot_token = '5119865265:AAGo4fCkoRE9J2oXk6Yt7wVwLOo'
-    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    data = {'chat_id': chat_id, 'text': message}
-    requests.post(url, data=data)
